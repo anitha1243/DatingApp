4eba0818530bd2af04f43ccba5f4476e5b8fe924
Anitha Kurian <anitha_kurian@dialog.com.au>
2023-03-17 00:48:23 +1100

    end of section 2

---
+++ API/Program.cs
@@ -0,0 +1,21 @@
+using API.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>(opt => 
+{
+    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.MapControllers();
+
+app.Run();
