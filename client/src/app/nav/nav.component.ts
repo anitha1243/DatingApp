7c3cfb521d8fe09871c0e966059bbe7e7e5ff39e
Anitha Kurian <anitha_kurian@dialog.com.au>
2023-03-30 22:41:44 +1100

    end of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable, of } from 'rxjs';
+import { User } from '../_models/user';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+  model: any = {};
+  currentUser$: Observable<User | null> = of(null);
+
+  constructor(public accountService : AccountService, private router: Router,
+    private toaster: ToastrService) { }
+
+  ngOnInit(): void {
+    this.currentUser$ = this.accountService.currentUser$;
+  }
+
+  login(){
+    this.accountService.login(this.model).subscribe({
+      next: _ => this.router.navigateByUrl('/members'),
+      error: error => this.toaster.error(error.error)
+    });
+  }
+
+  logout() {
+    this.accountService.logout();
+    this.router.navigateByUrl('/');
+  }
+}
---
+++ client/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { AccountService } from '../_services/account.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Output() cancelRegister = new EventEmitter();
+  model: any = {}
+
+  constructor(private accountService: AccountService, private toaster: ToastrService) { }
+
+  ngOnInit(): void {
+  }
+
+  register(){
+    this.accountService.register(this.model).subscribe({
+      next: response => {
+        this.cancel();
+      },
+      error: error =>
+      {
+        this.toaster.error(error.error);
+        console.log(error);
+      }
+    })
+  }
+
+  cancel(){
+    this.cancelRegister.emit(false);
+  }
+}
